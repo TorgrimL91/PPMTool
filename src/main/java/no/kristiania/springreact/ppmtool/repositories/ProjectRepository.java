09097fb47e921f2d2d9bd3aa8acb175e9fc64fee
Torgrim <lautor17@student.westerdals.no>
2019-05-16 07:54:51 +0200

    masse nytt

---
+++ src/main/java/no/kristiania/springreact/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package no.kristiania.springreact.ppmtool.repositories;
+
+import no.kristiania.springreact.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+   Project findByProjectIdentifier(String projectId);
+
+    @Override
+    Iterable<Project> findAll();
+}
---
+++ src/main/java/no/kristiania/springreact/ppmtool/services/ProjectService.java
@@ -0,0 +1,50 @@
+package no.kristiania.springreact.ppmtool.services;
+
+import no.kristiania.springreact.ppmtool.domain.Project;
+import no.kristiania.springreact.ppmtool.exceptions.ProjectIdException;
+import no.kristiania.springreact.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exists");
+
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return  projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete project with ID '" + projectId + "'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+}
