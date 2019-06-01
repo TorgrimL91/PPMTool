ca5ec93bb0c57e6c88f2ad9f7150f692284f34b2
Torgrim <lautor17@student.westerdals.no>
2019-06-01 12:03:20 +0200

    added exception handling

--- src/main/java/no/kristiania/springreact/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ src/main/java/no/kristiania/springreact/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.jws.WebResult;
-
 @RestController
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -26,6 +24,12 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request){
+        UsernameAlreadyExistsException exceptionResponse = new UsernameAlreadyExistsException(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ src/main/java/no/kristiania/springreact/ppmtool/exceptions/UsernameAlreadyExistsResponse.java
@@ -0,0 +1,20 @@
+package no.kristiania.springreact.ppmtool.exceptions;
+
+
+
+public class UsernameAlreadyExistsResponse {
+
+    private String username;
+
+    public UsernameAlreadyExistsResponse(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
